6d3e80a17c74bd14ea1277dbf81d53e4dd594dd7
LaurynasSabutis <laurynas.sabutis@stud.ktmc.lt>
2025-10-22 17:31:26 +0300

    Pridetas login bei sign up

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,29 @@ import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Bookmarks from "./pages/Bookmarks"
 import Movies from "./pages/Movies"
 import TVSeries from "./pages/TVSeries"
 
+=======
+import Bookmarks from "./pages/bookmarks/Bookmarks"
+import Login from "./pages/Login/Login"
+import Signup from "./pages/Signup/Signup"
+import Navbar from "./components/Navbar"
+
+const AppLayout = () => (
+  <div className='root-container'>
+    <div className='nav-container'>
+      <Navbar />
+    </div>
+
+    <div className='root-body'>
+      <Outlet />
+    </div>
+  </div>
+)
+>>>>>>> 36bbc2c (Pridetas login bei sign up)
 
 function App() {
 // Add routes here - Armandas
@@ -39,6 +58,10 @@ function App() {
       path: "/login",
       element: <Login />
 >>>>>>> 658311f (Sukurtas login bei sign up)
+    },
+    {
+      path: "/signup",
+      element: <Signup />
     },
     {
       // 404 not found page
